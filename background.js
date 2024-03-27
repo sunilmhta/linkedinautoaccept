ccba2b48bd68b85af183182f1030d1edb188d4e5
Sunil Kumar <sunilkr.kumar26@gmail.com>
2024-03-27 19:19:59 +0530

    Add chrome extension to auto

---
+++ background.js
@@ -0,0 +1,4 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('LinkedIn Auto Accept extension installed!');
+  });
+  
---
+++ content.js
@@ -0,0 +1,5 @@
+const acceptButtons = document.querySelectorAll('.invitation-card__action-btn');
+
+acceptButtons.forEach(button => {
+  button.click();
+});
